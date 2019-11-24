f3a08f05f8bc15d5f0ec9dffa019a9ffcd89db28
Fabien FERT <fabien.fert@wanadoo.fr>
2019-11-24 11:32:50 +0100

    Add files via upload
    Version de base du fichier permettant d'ouvrir une console de debug pour les programmes Delphi VCL ou FMX. (il permet d'envoyer environ 8500 lignes dans une console texte par la commande TDebug.Write('Mon texte à envoyer...');
    Nota: La fermeture de la console ferme l'application hôte.

---
+++ ConsoleDebug.pas
@@ -0,0 +1,41 @@
+// *********************************************************************************************************************
+// Auteur : Fabien FERT
+// Date : 01/11/2019
+// Licence : MIT
+// *********************************************************************************************************************
+
+unit ConsoleDebug;
+
+interface
+
+uses
+  {$IFDEF MSWINDOWS}
+     Winapi.Windows,
+  {$ENDIF}
+  System.SysUtils, System.Types, System.UITypes, System.Classes;
+
+type
+  TDebug = class
+  public
+    class procedure Write(aStr : string);
+  end;
+
+implementation
+
+class procedure TDebug.Write(aStr : string);
+begin
+
+  {$IFDEF MSWINDOWS}
+    if DebugHook <> 0 then
+      writeLn(formatDateTime('yyyy-mm-dd hh:nn:ss', now) + ' - ' + aStr);
+  {$ENDIF}
+end;
+
+
+Initialization
+  // Windows Console can show about 8500 lines
+  {$IFDEF MSWINDOWS}
+    AllocConsole;
+  {$ENDIF}
+
+end.
